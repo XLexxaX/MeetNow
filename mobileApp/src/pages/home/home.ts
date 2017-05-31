d24720281485d29f86bd518311a60822c2d6b2e0
XLexxaX <al95@gmx.net>
2017-05-31 13:07:42 +0200

    Home screen

--- mobileApp/src/pages/home/home.ts
+++ mobileApp/src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  selectedItem: any;
+  items: Array<{title: string, id: number, icon: string}>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('item');
 
+    this.items = [];
+    for (let i = 1; i < 11; i++) {
+      this.items.push({
+        title: 'Item ' + i,
+        id: i,
+        icon: 'calendar'
+      });
+    }
+  }
+
+  itemTapped(event, item) {
+    // That's right, we're pushing to ourselves!
+    this.navCtrl.push(HomePage, {
+      item: item
+    });
   }
 
 }
