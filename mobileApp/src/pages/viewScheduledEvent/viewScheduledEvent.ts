5720887f712437144ac41a18f43a86ac46a85d3d
Alex <al95@gmx.net>
2017-05-31 22:31:04 +0200

    Rest-service to movie-db

--- mobileApp/src/pages/viewScheduledEvent/viewScheduledEvent.ts
+++ mobileApp/src/pages/viewScheduledEvent/viewScheduledEvent.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {RESTService} from '../services/RESTService';
+
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'viewScheduledEvent.html'
+  templateUrl: 'viewScheduledEvent.html',
+  providers: [RESTService]
 })
+
 export class ViewScheduledEventPage {
 
-  constructor(public navCtrl: NavController) {
-    // If we navigated to this page, we will have an item available as a nav param
+  items: Array<any>;
 
+  constructor(private navController: NavController, private restService: RESTService) {
+    this.restService.get('The Hobbit: An Unexpected Journey').subscribe(
+      data => {
+        this.items = data.results;
+        console.log(data);
+      },
+      err => {
+        console.log(err);
+      },
+      () => console.log('Movie Search Complete')
+    );
   }
-
 
 }
