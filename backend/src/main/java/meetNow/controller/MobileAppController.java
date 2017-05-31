227873229e9d45c6d494d171c83930e183f8af93
Florian Bunsmann <fbunsmann@web.de>
2017-05-31 12:04:51 +0100

    swagger code generation added

--- backend/src/main/java/meetNow/controller/MobileAppController.java
+++ backend/src/main/java/meetNow/controller/MobileAppController.java
@@ -1,14 +1,23 @@
 package meetNow.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import api.api.EnterAreaApi;
+
 @RestController
-public class MobileAppController {
+public class MobileAppController implements EnterAreaApi {
 
 	@RequestMapping("/test")
-    public String greeting(String test) {
-        return "OK";
-    }
+	public String greeting(String test) {
+		return "OK";
+	}
+
+	@Override
+	public ResponseEntity<Void> enterArea(Integer areaId) {
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
 }
