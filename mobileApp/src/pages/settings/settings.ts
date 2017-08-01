87055580c145e3ba848130f00b1260179b5c4fb4
Florian Bunsmann <fbunsmann@web.de>
2017-08-01 20:29:01 +0200

    settings developed

--- mobileApp/src/pages/settings/settings.ts
+++ mobileApp/src/pages/settings/settings.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,10 +7,42 @@ import { NavController } from 'ionic-angular';
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController) {
-    // If we navigated to this page, we will have an item available as a nav param
+  private status: boolean;
+  private isEnabled: boolean;
+  private bgGeo = (<any>window).BackgroundGeolocation;
+  private toggleValue = false;
 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // If we navigated to this page, we will have an item available as a nav param
+    this.isEnabled = false;
+    let that = this;
+
+    this.bgGeo.getState(function (state) {
+      that.isEnabled = true;
+      that.status = state.enabled;
+      that.toggleValue = state.enabled;
+    });
   }
 
 
+  private handleGeolocations() {
+    this.status = !this.status;
+    this.isEnabled = false;
+    let that = this;
+    if (this.status) {
+      this.bgGeo.startGeofences(function (state) {
+        console.log('Geofence monitoring started', state.trackingMode);
+        that.isEnabled = true;
+      });
+    } else {
+      this.bgGeo.stop(function (state) {
+        console.log('Geofence monitoring stopped', state.trackingMode);
+        that.isEnabled = true;
+      });
+    }
+  }
+
+  private updateView() {
+
+  }
 }
